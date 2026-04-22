5f344aba715220fa8f2c05091f3ae1ef7c35be05
John Keroles <johnkeroles@Johns-MacBook-Pro-3.local>
2026-04-22 09:29:03 -0700

    Lab#13

--- game.c
+++ game.c
@@ -14,33 +14,7 @@
 #include <unistd.h>
 
 //place function prototypes here
-void jgFun(void);
-
-void jbInit(void);
-
-void ip21(void);
-
-
-void folksamEV(void);
-
-void ip21(void);
-
-void rtFun(void);
-
-void JLeyva(void);
-
-void fish(void);
-
-void printStudent30(void);
-
-void printInitialsAjewett(void);
-
-void jgFun(void);
-
-
-
-
-
+void JKFun(void);
 
 
 
@@ -99,19 +73,16 @@ int main(int argc, char *argv[])
 			}
 			case 8:
 			{
-				folksamEV();
 				puts("room8");
 				break;
 			}
 			case 9:
 			{
-				puts("room9");
-				printInitialsAjewett();
+				puts("room9"); 
 				break;
 			}
 			case 10:
 			{
-				fish();
 				puts("room10");
 				break;
 			}
@@ -147,7 +118,6 @@ int main(int argc, char *argv[])
 			}
 			case 17:
 			{
-				jbInit();
 				puts("room17");
 				break;
 			}
@@ -168,7 +138,6 @@ int main(int argc, char *argv[])
 			}
 			case 21:
 			{
-				ip21();
 				puts("room21");
 				break;
 			}
@@ -215,7 +184,6 @@ int main(int argc, char *argv[])
 			case 30:
 			{
 				puts("room30");
-				printStudent30();
 				break;
 			}
 			case 31:
@@ -270,7 +238,7 @@ int main(int argc, char *argv[])
 			}
 			case 42:
 			{
-				jgFun();
+				JKFun(); 
 				puts("room42");
 				break;
 			}
@@ -341,13 +309,11 @@ int main(int argc, char *argv[])
 			}
 			case 56:
                         {
-                                JLeyva();
 				puts("room56");
                                 break;
                         }
 			case 57:
                         {
-				rtFun();
                                 puts("room57");
                                 break;
                         }
@@ -458,52 +424,7 @@ int main(int argc, char *argv[])
 
 //place functions here
 
-void jgFun(void)
+void JKFun(void)
 {
-	printf("Finish Lab#13"); 
-=======
-void folksamEV(void) 
-{
-	printf("EV\n");
-}
-
-void jbInit()
-{
-	puts("jbRoom17");
-}
-
-void ip21(void)
-{
-	printf("IP\n");
-}
-
-void rtFun(void)
-{
-	printf("RT");
-}
-
-void JLeyva(void)
-{
-	printf("JL");
-}
-
-void fish(void) 
-{
-	printf("JF");
-}
-
-void printStudent30(void)
-{
-	printf("Student initials CP\n");
-	return;
-}
-void printInitialsAjewett(void)
-{
-	printf("AJ\n");
-}
-
-void jgFun(void)
-{
-	printf("JGB");	
-
+	printf("Lab#13"); 
 }
